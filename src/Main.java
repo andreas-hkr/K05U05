9fa5e08788246bae0277ca7c41afb11cc205c196
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-15 18:24:26 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Skriv ut en multiplikationstabell med X rader som ser ut så här:
@@ -5,6 +7,15 @@ public class Main {
         // 2 4
         // 3 6 9
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ange antal rader: ");
+        int rows = input.nextInt();
 
+        for (int i=1; i <= rows; i++) {
+            for (int j=1; j <= i; j++) {
+                System.out.print(i*j + "  ");
+            }
+            System.out.println();
+        }
     }
 }
