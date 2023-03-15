a16b76ef0a5f74dea250aad2b4569024689bf6b2
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-15 18:13:35 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skriv ut en multiplikationstabell med X rader som ser ut så här:
+        // 1
+        // 2 4
+        // 3 6 9
+
+
+    }
+}
